cc403650ec20d11a76743da8156924d109ec459b
Sumaya Akther Shammee <37858535+Shammee2596@users.noreply.github.com>
2019-11-20 18:15:26 +0600

    v3 auth recycler view added

--- ContactAppv3/app/src/main/java/com/example/contactapp_v3/MainActivity.java
+++ ContactAppv3/app/src/main/java/com/example/contactapp_v3/MainActivity.java
@@ -1,14 +1,191 @@
 package com.example.contactapp_v3;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager.widget.ViewPager;
 
+import android.Manifest;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.contactapp_v3.models.Contact;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.firebase.ui.database.SnapshotParser;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+
+    //private TabAdapter adapter;
+    //private TabLayout tabLayout;
+    private ViewPager viewPager;
+    private RecyclerView recyclerView;
+    private View v;
+    private LinearLayoutManager linearLayoutManager;
+    private FirebaseRecyclerAdapter adapter;
+
+    int ALL_PERMISSIONS = 101;
+    final String[] permissions = new String[]{Manifest.permission.READ_CONTACTS,
+            Manifest.permission.READ_CALL_LOG, Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.CALL_PHONE, Manifest.permission.WRITE_CONTACTS};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ActivityCompat.requestPermissions(this, permissions, ALL_PERMISSIONS);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        LayoutInflater inflater = getLayoutInflater();
+
+        //v = inflater.inflate(R.layout.frag_contacts,container,false);
+        recyclerView = findViewById(R.id.rv_contacts);
+        linearLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(linearLayoutManager);
+        recyclerView.setHasFixedSize(true);
+
+
     }
+
+    private void addContact(FirebaseUser currentUser) {
+
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Contact")
+                .child("User").child(currentUser.getUid()).push();
+
+        Contact contact = new Contact("Name", "0123456789", "asd@gdfd.cvc");
+        reference.setValue(contact);
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser == null) {
+            sendToStartPage();
+        }else {
+            //addContact(currentUser);
+            fetch();
+            adapter.startListening();
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        //adapter.stopListening();
+    }
+
+    private void sendToStartPage() {
+        Intent intent = new Intent(MainActivity.this, StartActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        super.onOptionsItemSelected(item);
+        if (item.getItemId() == R.id.menu_log_out) {
+            FirebaseAuth.getInstance().signOut();
+            sendToStartPage();
+        }
+        return true;
+    }
+
+
+    private void fetch() {
+        Query query = FirebaseDatabase.getInstance().getReference("Contact")
+                .child("User").child(FirebaseAuth.getInstance().getCurrentUser().getUid()).orderByChild("name")
+                .startAt("").endAt("" + "\uf8ff");
+
+        FirebaseRecyclerOptions<Contact> options =
+                new FirebaseRecyclerOptions.Builder<Contact>()
+                        .setQuery(query, Contact.class)
+                        .setLifecycleOwner(this)
+                        .build();
+
+        adapter = new FirebaseRecyclerAdapter<Contact, ViewHolder>(options) {
+            @Override
+            public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+                View view = LayoutInflater.from(parent.getContext())
+                        .inflate(R.layout.items_contacts, parent, false);
+
+                final ViewHolder viewHolder = new ViewHolder(view);
+
+                viewHolder.item_contact.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Toast.makeText(MainActivity.this, "Clicked", Toast.LENGTH_LONG).show();
+                    }
+                });
+
+                return new ViewHolder(view);
+            }
+
+
+            @Override
+            protected void onBindViewHolder(ViewHolder holder, final int position, Contact model) {
+                holder.setName(model.getName());
+                holder.setNumber(model.getNumber());
+            }
+
+        };
+        recyclerView.setAdapter(adapter);
+    }
+
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        TextView txtName, txtNumber;
+        Button button;
+
+        LinearLayout item_contact;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            item_contact = itemView.findViewById(R.id.contact_item);
+            txtName = itemView.findViewById(R.id.contact_name);
+            txtNumber = itemView.findViewById(R.id.number);
+            button = itemView.findViewById(R.id.contact_button);
+        }
+
+        public void setName(String name) {
+            txtName.setText(name);
+        }
+
+        public void setNumber(String number) {
+            txtNumber.setText(number);
+        }
+
+    }
+
 }
---
+++ ContactAppv3/app/src/main/java/com/example/contactapp_v3/models/Contact.java
@@ -0,0 +1,168 @@
+package com.example.contactapp_v3.models;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import androidx.annotation.Nullable;
+
+public class Contact implements Comparable<Contact>,Parcelable {
+    private String name, number;
+    private String email;
+    private String label;
+    private String lastName;
+    private String imageUrl;
+    private String userId;
+    private boolean isFavourite = false;
+    private String _id;
+
+    public Contact() {}
+
+    public Contact(String name, String number, String email) {
+        this.name = name;
+        this.number = number;
+        this.email = email;
+    }
+
+    public Contact(String name, String number, String email, boolean isFavourite) {
+        this.name = name;
+        this.number = number;
+        this.email = email;
+        this.isFavourite = isFavourite;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public boolean isFavourite() {
+        return isFavourite;
+    }
+
+    public void setFavourite(boolean favourite) {
+        isFavourite = favourite;
+    }
+
+    public String get_id() {
+        return _id;
+    }
+
+    public void set_id(String _id) {
+        this._id = _id;
+    }
+
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(this.name);
+        dest.writeString(this.number);
+        dest.writeString(this.email);
+        dest.writeString(this.label);
+        dest.writeString(this.lastName);
+        dest.writeString(this.imageUrl);
+        dest.writeString(this.userId);
+        dest.writeByte(this.isFavourite ? (byte) 1 : (byte) 0);
+        dest.writeString(this._id);
+    }
+
+    protected Contact(Parcel in) {
+        this.name = in.readString();
+        this.number = in.readString();
+        this.email = in.readString();
+        this.label = in.readString();
+        this.lastName = in.readString();
+        this.imageUrl = in.readString();
+        this.userId = in.readString();
+        this.isFavourite = in.readByte() != 0;
+        this._id = in.readString();
+    }
+
+    public static final Parcelable.Creator<Contact> CREATOR = new Parcelable.Creator<Contact>() {
+        @Override
+        public Contact createFromParcel(Parcel source) {
+            return new Contact(source);
+        }
+
+        @Override
+        public Contact[] newArray(int size) {
+            return new Contact[size];
+        }
+    };
+
+    @Override
+    public int compareTo(Contact u) {
+        return name.toLowerCase().compareTo(u.name.toLowerCase());
+    }
+
+    @Override
+    public boolean equals(@Nullable Object u) {
+        Contact c = (Contact) u;
+        return number.equals(c.number) && name.equals(c.name);
+    }
+
+    @Override
+    public int hashCode() {
+        long hash = 0;
+        for (int i = 0; i < number.length(); i++) {
+            hash = hash * 347L + number.charAt(i) ;
+            hash %= 1000000007L;
+        }
+        return (int) hash;
+    }
+}
